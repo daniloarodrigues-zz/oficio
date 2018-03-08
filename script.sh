7418c3179fd194a8e790f8293e7d3bec18519055
Danilo Rodrigues <danilorodrigues@nivsoft.com.br>
2018-03-08 13:09:45 -0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+RUN apt update -y;
+RUN apt-install vim python3 python pip virtualenv build-essential python3-dev python -y;
+
+mkdir /deploy;
+mkdir /deploy/App;
+mkdir /deploy/envs;
+cd /deploy/envs;
+
+RUN virtualenv -p python3 envoficio;
+RUN source envoficio/bin/activate;
+RUN pip install django pillow xhtml2pdf;
