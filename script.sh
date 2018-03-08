6067c6f47b17d812d797ca929187e645389dd513
Danilo Rodrigues <danilorodrigues@nivsoft.com.br>
2018-03-08 14:01:25 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,10 +3,18 @@ apt update && apt upgrade -y
 apt install vim python3 python virtualenv build-essential python3-dev python -y
 apt install python-pip -y
 
+DIR=/
+FILE=deploy
+
+if [ -e "$DIR$FILE" ] ; then
+echo "Os diretórios já existem"
+else
 mkdir /deploy
 mkdir /deploy/App
 mkdir /deploy/envs
+fi
+
 cd /deploy/envs
 virtualenv -p python3 envoficio
 source envoficio/bin/activate
-pip install django pillow xhtml2pdf
+pip install django pillow xhtml2pdf gunicorn
