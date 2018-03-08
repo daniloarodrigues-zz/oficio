d4a4702d1c1c5169c7db4c2f5e2322188cbe2e5b
Danilo Rodrigues <danilorodrigues@nivsoft.com.br>
2018-03-08 13:35:34 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 apt update && apt upgrade -y
-apt install vim python3 python pip virtualenv build-essential python3-dev python
+apt-get install vim python3 python pip virtualenv build-essential python3-dev python -y
 
-mkdir deploy
-mkdir deploy/App
-mkdir deploy/envs
-cd deploy/envs
+mkdir /deploy
+mkdir /deploy/App
+mkdir /deploy/envs
+cd /deploy/envs
 virtualenv -p python3 envoficio
 source envoficio/bin/activate
 pip install django pillow xhtml2pdf
